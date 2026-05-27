fa451ae1466752ebe19cc16539ade80ad74cb06e
YitongTseo <yitong.tseo@gmail.com>
2026-05-27 05:28:33 +0000

    v6/viewer: move focus.js to focus/index.js (no behavior change)

--- v6/viewer/focus/index.js
+++ v6/viewer/focus/index.js
@@ -1,3 +1,4 @@
+// entrypoint — modules will be extracted from this file one at a time
 import * as THREE from 'three';
 import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
